6bc4c9a7c388ea5b3a5138e987f55c47d87a79a7
zhekaBeast <gdflz12345@gmail.com>
2025-10-27 16:31:15 +0300

    Correct FavoriteScreen.kt

--- app/src/main/java/com/example/musicapp/ui/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/example/musicapp/ui/favorite/FavoriteScreen.kt
@@ -1,7 +1,9 @@
 package com.example.musicapp.ui.favorite
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
+@Composable
 fun FavoriteScreen(navController: NavHostController, modifier: Modifier = Modifier) {
 }
